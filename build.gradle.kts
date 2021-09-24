bf76422433682e024ef5fc4fd7e9946e84dc477f
Hiroshi Miura <miurahr@linux.com>
2021-09-24 19:51:48 +0900

    Change sonatype OSSRH server to the older one
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -118,8 +118,8 @@ nexusPublishing {
     repositories {
         sonatype {
             stagingProfileId.set(properties["stagingProfileId"]?.toString())
-            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+            nexusUrl.set(uri("https://oss.sonatype.org/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://oss.sonatype.org/content/repositories/snapshots/"))
             username.set(System.getenv("SONATYPE_USER"))
             password.set(System.getenv("SONATYPE_PASS"))
         }
